f223eacbf0b0612764da4f822e3eca5ce0a59e93
Simon Jefford <simon.jefford@gmail.com>
2009-04-30 17:11:44 +0100

    Externals converted

--- convertexternals.rb
+++ convertexternals.rb
@@ -1,5 +1,13 @@
 require 'yaml'
+require 'fileutils'
+include FileUtils
 hash = YAML::load_file("config/externals.yml")
+homepath = `pwd`
 hash.keys.each do |path|
-  puts path
+  rm_rf path
+  system "git clone #{hash[path][:repository]} #{path}"
+  cd path do
+    system "git co #{hash[path][:revision]}"
+    rm_rf ".git"
+  end
 end
