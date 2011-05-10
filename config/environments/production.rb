5521fce8197f08dc2b244c8b7620fe67ef39b02d
Simon Jefford <simon.jefford@gmail.com>
2011-05-10 20:48:17 +0100

    Revert "serve static assets please"
    This reverts commit 5031567a94abd2c3f1cb4ef230b347170e36238e.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,6 @@ config.cache_classes = true
 config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
-config.serve_static_assets                           = true
 
 # Use a different cache store in production
 # config.cache_store = :mem_cache_store
